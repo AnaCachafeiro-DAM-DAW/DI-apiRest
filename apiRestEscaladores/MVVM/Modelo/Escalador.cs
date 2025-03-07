fa6a56a5b0ed5725fa29dca7fbf2c4085e204483
AnaCachafeiro-DAM <anacs13@educastur.es>
2025-03-07 17:22:39 +0100

    070325 Funcionando

--- apiRestEscaladores/MVVM/Modelo/Escalador.cs
+++ apiRestEscaladores/MVVM/Modelo/Escalador.cs
@@ -1,14 +1,28 @@
+using System.Text.Json.Serialization;
+
 namespace apiRestEscaladores.MVVM.Modelo;
 
 
 // tenemos que pegarlo del data del mockapi
 public class Escalador
 {
+
+    [JsonPropertyName("name")]
     public string Name { get; set; }
+    
+    [JsonPropertyName("age")]
     public int Age { get; set; }
+  
+    [JsonPropertyName("mountainGroup")]
     public string MountainGroup { get; set; }
+    
+    [JsonPropertyName("experience")]
     public int Experience { get; set; }
+   
+    [JsonPropertyName("federado")]
     public bool Federado { get; set; }
+    
+    [JsonPropertyName("id")]
     public String Id { get; set; }
 
     public string FederadoTexto
@@ -19,7 +33,10 @@ public class Escalador
         }
     }
 
-    
+
+
+
+
 }
 
 
